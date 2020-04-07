bc5830c2f446cb0c52c8f24a448afe0859a62241
mohamedmagdy5061 <mohamedmagdy5061@gmail.com>
2020-04-07 11:23:07 +0200

    fix native-base issue by downgrade virsion

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
-import Logo from '../components/Logo'
+import {
+  Container,
+  Header,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label
+} from 'native-base';
 
+import Logo from '../components/Logo';
 
 const LoginScreen = () => {
-    return(
-        <View>
-            <Logo />
+  return (
+    <View>
+      <Logo />
 
-            <View >
-                <TextInput placeholder='User Name'/>
-                <TextInput placeholder='Password' />
-                <Button title='Login'/>
-            </View>
+      <View>
+        <TextInput placeholder="User Name" />
+        <TextInput placeholder="Password" />
+        <Button title="Login" />
+      </View>
+      <View>
+          <Form>
+            <Item floatingLabel>
+              <Label style={{color:'green'}}>Username</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          </Form>
+      </View>
+    </View>
+  );
+};
 
-        </View>
-    )
+const styles = StyleSheet.create({});
 
-}
-
-
-const styles = StyleSheet.create({
-
-})
-
-
-export default LoginScreen
+export default LoginScreen;
